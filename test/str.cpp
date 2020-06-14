636c4f365e61a194fe59e98176fc31e22aaed27a
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2020-06-15 03:39:56 +0900

    例を修正

--- test/str.cpp
+++ test/str.cpp
@@ -1,8 +1,9 @@
 #include <gtest/gtest.h>
 
 TEST(StrCompareTest, compareTest) {
-	char* val1 = "hoge\0fuga";
-	char* val2 = "hoge";
+	char val1[20] = "hoge";
+	char val2[20] = "hoge";
+	std::cout << sizeof(val1) << ", " << sizeof(val2) << std::endl;
 	EXPECT_EQ(val1, val2);		// ‚±‚ê‚Í‚±‚¯‚é
 	EXPECT_STREQ(val1, val2);	// ‚±‚ê‚Í‚±‚¯‚È‚¢
 }
